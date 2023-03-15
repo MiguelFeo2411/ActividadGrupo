fd0651bc1a1d8fff4714643887f9569b9afec2a9
Miguel Feo <mfemerchan@gmail.com>
2023-03-14 21:12:46 -0500

    Metodo Resta

--- src/Resta.java
+++ src/Resta.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Resta {
+public class Resta {
+    private void Resta(int Numero1, int Numero2, int Numero3)
+    {
+        System.out.println(Numero1+Numero2+Numero3);
+    }
 }
