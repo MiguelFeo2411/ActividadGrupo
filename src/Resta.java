384500f59df5881f4240563e6f636f74308401d9
Miguel Feo <mfemerchan@gmail.com>
2023-03-15 17:34:47 -0500

    Cambiar el signo de suma a multiplicacion

--- src/Resta.java
+++ src/Resta.java
@@ -1,6 +1,2 @@
-public class Resta {
-    private void Resta(int Numero1, int Numero2, int Numero3)
-    {
-        System.out.println(Numero1+Numero2+Numero3);
-    }
+package PACKAGE_NAME;public class Resta {
 }
