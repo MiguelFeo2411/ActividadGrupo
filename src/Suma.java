38e3cbf3db20375d3086978df662ef8b4667d354
Tom1684 <tomas16oficial@hotmail.com>
2023-03-15 20:12:01 -0500

    Suprime un numero

--- src/Suma.java
+++ src/Suma.java
@@ -1,8 +1,8 @@
 public class Suma {
 
-    private void Sumador(int x, int Numero1, int Numero2, int Numero3){
+    private void Sumador(int x, int Numero2, int Numero3){
 
-        x= (Numero1+Numero2+Numero3);
+        x= (Numero2+Numero3);
 
         System.out.println("El numero es "+x);
 
